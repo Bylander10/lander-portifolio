a2e8f0064e1d52fc8513b6a5a0fc5a2e1257db25
Bylander <joao.amb23@gmail.com>
2024-08-13 16:05:41 -0300

    nav structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// components
+import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* logo */}
+            <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    Lander <span className="text-accent">.</span>
+                </h1>
+            </Link>
+            {/* desktop nav */}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav />
+                <Link href="/contact">
+                    <Button>Pingas</Button>
+                </Link>
+            </div>
+
+            {/* mobile nav */}
+            <div className="xl:hidden">mobile nav</div>
+        </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
+
