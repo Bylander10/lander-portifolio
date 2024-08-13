830d059502b64b02440c2f131efb0bc8cb505307
Bylander <joao.amb23@gmail.com>
2024-08-13 19:55:20 -0300

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "resume",
+        path: "/resume",
+    },
+    {
+        name: "work",
+        path: "/work",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    },
+];
+
 const Nav = () => {
-  return <nav>desktop nav</nav>;
+  return <nav>
+    {links.map((link, index) => {
+        return (
+            <Link href={link.path} key={index}>
+                {link.name}
+            </Link>
+        )
+    })}
+  </nav>;
 };
 
-export default Nav
+export default Nav;
