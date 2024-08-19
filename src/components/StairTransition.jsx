fb9d5adb43c8e29afac0df820be7d934e8fa99a4
lander2 <joao.amb23@gmail.com>
2024-08-19 16:46:43 -0300

    update

--- src/components/StairTransition.jsx
+++ src/components/StairTransition.jsx
@@ -1,6 +1,19 @@
+"use client"
+
+import { AnimatePresence, motion } from "framer-motion"
+import { usePathname } from "next/navigation"
+
+
 const StairTransition = () => {
+  const pathname = usePathname()
   return (
-    <div>StairTransition</div>
+    <>
+      <AnimatePresence mode="wait">
+        <div key={pathname}>
+          <div className="h-screen w-screen fixed top-0">stairs</div>
+        </div>
+      </AnimatePresence>
+    </>
   )
 }
 
