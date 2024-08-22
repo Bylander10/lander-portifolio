9301b07abc90e64fbb7821ef17a6aa086a8fad3f
lander2 <joao.amb23@gmail.com>
2024-08-22 13:48:34 -0300

    update stairs

--- src/components/StairTransition.jsx
+++ src/components/StairTransition.jsx
@@ -2,6 +2,7 @@
 
 import { AnimatePresence, motion } from "framer-motion"
 import { usePathname } from "next/navigation"
+import Stairs from "./Stairs"
 
 
 const StairTransition = () => {
@@ -10,7 +11,9 @@ const StairTransition = () => {
     <>
       <AnimatePresence mode="wait">
         <div key={pathname}>
-          <div className="h-screen w-screen fixed top-0">stairs</div>
+          <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+            <Stairs />
+          </div>
         </div>
       </AnimatePresence>
     </>
