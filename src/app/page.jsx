b0a4891adfb2608708109d3779070e0e63f5d351
lander2 <joao.amb23@gmail.com>
2024-09-02 16:49:37 -0300

    homepage start

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,24 @@
-export default function Home() {
-  return <main>homepage</main>;
- 
+import { Button } from "@/components/ui/button";
+import { fiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          { /* text */ }
+          <div>
+            <span>Frontend Developer</span>
+            <h1>
+              Hello I'm <br /> <span>Bylander</span>
+            </h1>
+          </div>
+          { /* photo */ }
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
