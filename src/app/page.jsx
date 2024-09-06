c80f57521a3c959fd0e91557ac358cbdedb2c85f
Bylander <joao.amb23@gmail.com>
2024-09-05 22:28:37 -0300

    homepage frst

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,10 +8,13 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           { /* text */ }
           <div>
-            <span>Frontend Developer</span>
-            <h1>
-              Hello I'm <br /> <span>Bylander</span>
+            <span className="text-xl">Fullstack & Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Bylander</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+            </p>
           </div>
           { /* photo */ }
           <div>photo</div>
